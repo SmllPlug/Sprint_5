7644eeb24e6e532142648adb0884ac28055b50a7
Kirill Martyushov <martyushov.k.i.m@gmail.com>
2026-01-15 01:11:57 +0300

    Добавлены утилиты и тестовые данные

---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+class Constants:
+    AUTH_REQUIRED_LABEL = "Чтобы разместить объявление, авторизуйтесь"
+    TEST_TITLE = "Test"
+    TEST_PRODUCT_DESCRIPTION = "Продам"
+    TEST_PRICE = 10
+
+    ERROR_LABEL = "Ошибка"
