df2a74b6d93871441cd6b8670e8d36e61e7b93dd
Kirill Martyushov <martyushov.k.i.m@gmail.com>
2026-01-15 19:53:11 +0300

    Исправления замечаний в файле test_login.py: убраны шаги между ассертами

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,7 @@ from locators import MainPage, LoginPage
 from utils.user import TestUser
 from utils.waiters import Waiter
 
+
 class TestLogin:
     def test_login_valid_user(self, driver):
         # Переход на страницу входа
@@ -18,16 +19,11 @@ class TestLogin:
         driver.find_element(*LoginPage.PASSWORD).send_keys(TestUser.PASSWORD)
         # Нажатие кнопки войти 
         driver.find_element(*LoginPage.LOGIN_BUTTON).click()
-        # Проверка успешного входа на главную страницу
-        avatar_logo = WebDriverWait(
+        # Проверка успешного входа на главную страницу, логотип и аватар отображаюся
+        assert WebDriverWait(
             driver, Waiter.DEFAULT_WAIT_TIME).until(
             EC.visibility_of_element_located(MainPage.AVATAR_LOGO)
         )
-        # Проверка, что логотип аватара отображается
-        assert avatar_logo.is_displayed()
-        # Проверка, что имя пользователя и логотип аватара отображаются
-        user_element = WebDriverWait(
+        assert WebDriverWait(
             driver, Waiter.DEFAULT_WAIT_TIME).until(
-            EC.visibility_of_element_located(MainPage.USER)
-        )
-        assert user_element.text == "User."
+            EC.visibility_of_element_located(MainPage.USER)).text == "User."
